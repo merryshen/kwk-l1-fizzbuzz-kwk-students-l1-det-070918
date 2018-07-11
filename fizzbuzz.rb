5b902dcdb1766aec80280d771b363c9c876b9ce9
merryshen <yihuaca@gmail.com>
2018-07-11 15:18:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizz_buzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 != 0
     puts "Fizz"
-  elsif number % 5 == 0 
+  elsif number % 5 == 0 && number % 3 != 0
     puts "Buzz"
-  elsif number % 3 == 0 && number % 5 ==0
+  elsif number % 3 == 0 && number % 5 == 0
     puts "Fizz_Buzz"
 else 
     puts "nil"
